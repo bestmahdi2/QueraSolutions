144b7e9d4e5a657eabe23e3f3ed9665f11254485
bestmahdi2 <alibad979@gmail.com>
2022-02-11 19:37:02 +0330

    updated

--- Solved/bazi/bazi.py
+++ Solved/bazi/bazi.py
@@ -1,13 +1,21 @@
+# بازی
+# ID : 52543
+# https://quera.org/problemset/52543/
+# https://b2n.ir/m62054
+
 a = int(input())
 b = list(map(int, input().split()))
 m = ""
+
 for i in range(a):
     if len(b) >= 2:
         m += str(max(b)) + " "
         m += str(min(b)) + " "
         b.remove(max(b))
         b.remove(min(b))
+
     elif len(b) == 1:
         m += str(b[0]) + " "
         break
+
 print(m)
